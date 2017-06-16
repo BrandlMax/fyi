f31a80e72a3f65f18cf81dafa86b3f3d4fe1c08a
Maximilian Brandl <kontakt@brandl-maximilian.de>
2017-06-16 16:31:23 +0200

    Local ?

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 var express = require('express');
 var app = express();
 var server = app.listen(process.env.PORT || 1337);
-var heroku = true;
+var heroku = false;
 var os = require('os');
 var ifaces = os.networkInterfaces();
 
